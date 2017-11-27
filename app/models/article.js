52a2d2bde85b17d5d12ffd5244161bc4c9701dd0
Fredrick Mgbeoma <fredrick.mgbeoma@andela.com>
2017-11-27 20:21:16 +0100

    #153009251 setup unit testing for the front end (#15)
    * chore(frontend-testing): setup unit testing for the front end
    Fix merge conflict in files
    Ignore eslint on model files
    Fix indentation and eslint in article.js model file
    Finishes #153009251

---
+++ app/models/article.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-var, prefer-destructuring, object-shorthand, func-names, prefer-arrow-callback, max-len, import/no-unresolved */
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+* Article Schema
+*/
+var ArticleSchema = new Schema({
+  id: {
+    type: Number
+  },
+  title: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  content: {
+    type: String,
+    default: '',
+    trim: true
+  }
+});
+
+/**
+* Statics
+*/
+ArticleSchema.statics = {
+  load: function (id, cb) {
+    this.findOne({
+      id: id
+    }).select('-_id').exec(cb);
+  }
+};
+
+/**
+ * validate title and content
+ * if empty
+ */
+ArticleSchema.path('title').validate(function (title) {
+  return title.length;
+}, 'title cannot be blank');
+
+ArticleSchema.path('content').validate(function (content) {
+  return content.length;
+}, 'content cannot be blank');
+
+mongoose.model('Article', ArticleSchema);
---
+++ spec/sample.spec.js
@@ -0,0 +1,6 @@
+describe('Sample test for client', () => {
+  it('should return 4', (done) => {
+    expect(2 + 2).toEqual(4);
+    done();
+  });
+});
