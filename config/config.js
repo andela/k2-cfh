f519c09d50cea15d8621a1297bd1503928b1fb61
Henry Otighe <oti4me@gmailc.om>
2017-11-29 19:49:25 +0100

    chore(grunt to gulp) Convert from Grunt to Gulp taskrunner
    - install gulp
    - create gulpfile.js
    - configure gulpfile.js to start server
    - configure gulp to transpile from es6 to es5
    - update package.json to run gulp bundle and start the bundle file using npm start command
    - update server.js with es6 import statement to test for transpiling
    - run npm start to start server with gulp
    
    [Finished #153009252]

--- config/config.js
+++ config/config.js
@@ -1,7 +1,12 @@
-var _ = require('underscore');
+import _ from 'underscore';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const env = process.env.NODE_ENV || 'development';
+
 
 // Load app configuration
 
 module.exports = _.extend(
     require(__dirname + '/../config/env/all.js'),
-    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {});
+    require(__dirname + '/../config/env/' + env + '.json') || {});
