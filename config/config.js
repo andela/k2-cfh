5254f0f0f5ffb2bef12c67c1ef179f87fe8e4b75
Henry Otighe <oti4me@gmail.com>
2017-12-01 08:30:35 +0100

    chore(grunt to gulp) Convert from Grunt to Gulp taskrunner (#26)
    - install gulp
    - create gulpfile.js
    - configure gulpfile.js to start server
    - configure gulp to transpile from es6 to es5
    - update package.json to run gulp bundle and start the bundle file using npm start command
    - update server.js with es6 import statement to test for transpiling
    - run npm start to start server with gulp
    - fix hound warning

--- config/config.js
+++ config/config.js
@@ -1,7 +1,15 @@
-var _ = require('underscore');
+/* eslint-disable import/no-dynamic-require, global-require, import/no-unresolved, import/extensions, max-len */
+
+import _ from 'underscore';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+const env = process.env.NODE_ENV || 'development';
 
 // Load app configuration
 
 module.exports = _.extend(
-    require(__dirname + '/../config/env/all.js'),
-    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {});
+  require(path.join(__dirname, '/../config/env/all.js')),
+  require(path.join(__dirname, '/../config/env/', `${env}.json`)) || {}
+);
--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-/* eslint-disable max-len, no-var,vars-on-top, no-multi-assign, no-path-concat, prefer-template, func-names, prefer-arrow-callback, global-require, import/no-dynamic-require, prefer-destructuring, no-unused-vars  */
+/* eslint-disable import/no-dynamic-require, global-require, import/no-unresolved, import/extensions, max-len */
+
 /**
  * Module dependencies.
  */
-var express = require('express'),
-  fs = require('fs'),
-  passport = require('passport'),
-  logger = require('mean-logger'),
-  io = require('socket.io');
-require('dotenv').config();
+import io from 'socket.io';
+import express from 'express';
+import fs from 'fs';
+import passport from 'passport';
+import logger from 'mean-logger';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import paths from 'path';
+import config from './config/config';
+import auth from './config/middlewares/authorization';
+import routes from './config/routes';
+import expressConfig from './config/express';
+
+dotenv.config();
+
 /**
  * Main application entry file.
  * Please note that the order of loading is important.
  */
 
 // Load configurations
-// if test env, load example file
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
-  config = require('./config/config'),
-  auth = require('./config/middlewares/authorization'),
-  mongoose = require('mongoose');
-
-// Bootstrap db connection
-var db = mongoose.connect(config.db);
+mongoose.connect(config.db);
 
 // Bootstrap models
-var modelsPath = __dirname + '/app/models';
-var walk = function (path) {
-  fs.readdirSync(path).forEach(function (file) {
-    var newPath = path + '/' + file;
+const modelsPath = paths.join(__dirname, '/app/models');
+
+const walk = (path) => {
+  fs.readdirSync(path).forEach((file) => {
+    const newPath = paths.join(path, file);
     const stat = fs.statSync(newPath);
     if (stat.isFile()) {
       if (/(.*)\.(js|coffee)/.test(file)) {
@@ -43,29 +47,28 @@ walk(modelsPath);
 // bootstrap passport config
 require('./config/passport')(passport);
 
-var app = express();
+const app = express();
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next();
 });
 
 // express settings
-require('./config/express')(app, passport, mongoose);
+expressConfig(app, passport, mongoose);
 
 // Bootstrap routes
-require('./config/routes')(app, passport, auth);
+routes(app, passport, auth);
 
 // Start the app by listening on <port>
-var port = config.port;
-var server = app.listen(port);
-var ioObj = io.listen(server, { log: false });
+const { port } = config;
+const server = app.listen(port);
+const ioObj = io.listen(server, { log: false });
 // game logic handled here
 require('./config/socket/socket')(ioObj);
-
-// console.log('Express app started on port ' + port);
 
 // Initializing logger
 logger.init(app, passport, mongoose);
 
 // expose app
-exports = module.exports = app;
+exports = app;
+module.exports = app;
